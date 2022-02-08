e6253d3a2559a2bc21ee0dfc8a0001c480496897
Mattias Burkard <mattias.burkard@htl.rennweg.at>
2022-02-08 12:48:25 +0100

    Initial commit

---
+++ src/com/mathematti/plugpack/Plugpack.java
@@ -0,0 +1,15 @@
+package com.mathematti.plugpack;
+
+/**
+ * Main class for Plugpack
+ * @author Mathematti
+ */
+public class Plugpack {
+    /**
+     * Main method, currently only launches the main menu
+     * @param args args
+     */
+    public static void main(String[] args) {
+        MainMenu.launch(args);
+    }
+}
